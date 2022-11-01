ce82b90e8ab850033293cb3c55c115db2a7db16e
wiwi-git <duse34@hotmail.com>
2022-11-01 17:35:36 +0900

    livePhoto 저장용펑션  #1

---
+++ i wanna live photo/i wanna live photo/LivePhoto.swift
@@ -0,0 +1,37 @@
+//
+//  LivePhoto.swift
+//  i wanna live photo
+//
+//  Created by 위대연 on 2022/11/01.
+//
+
+import UIKit
+import Photos
+
+class LivePhoto {
+    struct LivePhotoResources {
+        var pairedImage: URL
+        var pairedVideo: URL
+    }
+    
+    var livePhotoResource: LivePhotoResources?
+    
+    func saveToLibrary(_ resource: LivePhotoResources) async throws -> Result<Bool, Error> {
+        return await withCheckedContinuation { continuation in
+            PHPhotoLibrary.shared().performChanges ({
+                let creationRequest = PHAssetCreationRequest.forAsset()
+                let options = PHAssetResourceCreationOptions()
+                
+                creationRequest.addResource(with: .pairedVideo, fileURL: resource.pairedVideo, options: options)
+                creationRequest.addResource(with: .photo, fileURL: resource.pairedImage, options: options)
+            }) { success, error in
+                if let error {
+                    print(error)
+                    continuation.resume(returning: .failure(error))
+                    return
+                }
+                continuation.resume(returning: .success(success))
+            }
+        }
+    }
+}
